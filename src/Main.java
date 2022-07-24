ba55201f27b00fa59f7ad8f67145e1e215097c36
Alexei Morozov <zevota89@gmail.com>
2022-07-24 16:48:58 +0300

    Calculator

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.util.concurrent.ForkJoinPool;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        MyThread thread = new MyThread(1);
+//        MyThread thread2 = new MyThread(2);
+//
+//        thread.start();
+//        thread2.start();
+
+        String folderPath = "C:/SOLIDWORKS Data (2)";
+        File file = new File(folderPath);
+
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
+    }
+//
+//    public static long getFolderSize(File folder) {
+//        if(folder.isFile()) {
+//            return folder.length();
+//        }
+//        long sum = 0;
+//        File[] files = folder.listFiles();
+//        for(File file : files) {
+//            sum += getFolderSize(file);
+//        }
+//        return sum;
+//    }
+}
